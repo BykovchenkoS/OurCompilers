8426688880f690cc549fdd8317ac21610920cf6e
Sonya Bykovchenko <sony.bykovchenko@gmail.com>
2024-11-11 16:41:07 +0300

    Assignment (operator or operation), arithmetic and logical operations with the possibility of changing the priority – bracket forms

--- OurLang.g4
+++ OurLang.g4
@@ -3,10 +3,37 @@ grammar OurLang;
 // начальное правило грамматики
 program: (statement)* EOF;
 
-statement: printStatement ;
+statement: printStatement            // оператор печати
+         | assignmentStatement       // оператор присваивания
+         ;
 
-printStatement: 'print' ' ' NUMBER ';' ;
+// оператор печати
+printStatement: 'print' expression ';' ;
+
+// оператор присваивания
+assignmentStatement: IDENTIFIER '=' expression ';' ;
+
+// выражения с приоритетом и операциями
+expression: expression op=(MUL | DIV) expression      # mulDivExpr
+          | expression op=(PLUS | MINUS) expression   # addSubExpr
+          | expression op=(AND | OR) expression       # logicalExpr
+          | '!' expression                            # notExpr
+          | '(' expression ')'                        # parenExpr
+          | NUMBER                                    # numberExpr
+          | IDENTIFIER                                # idExpr
+          ;
 
 // токены
-NUMBER          : [0-9]+ ;       // целые числа
-SPACE           : [ \r\n\t]+ -> skip;  // пропуск пробелов и переносов строк
+IDENTIFIER       : [a-zA-Z_] [a-zA-Z_0-9]* ;  // идентификаторы (переменные)
+NUMBER           : [0-9]+ ;                   // целые числа
+
+// операторы
+PLUS             : '+' ;
+MINUS            : '-' ;
+MUL              : '*' ;
+DIV              : '/' ;
+AND              : '&&' ;
+OR               : '||' ;
+NOT              : '!' ;
+
+SPACE            : [ \r\n\t]+ -> skip;  // пропуск пробелов и переводов строк
--- main.py
+++ main.py
@@ -1,27 +1,59 @@
 from antlr4 import *
 from util.OurLangLexer import OurLangLexer
-# from util.OurLangListener import OurLangListener
+from util.OurLangListener import OurLangListener
 from util.OurLangParser import OurLangParser
-from our.OurVisitor import OurVisitor
 import sys
 
+variables = {}
+
+class OurLangPrintListener(OurLangListener):
+    def exitPrintStatement(self, ctx):
+        # Обрабатываем выражение в операторе print
+        value = self.evaluate_expression(ctx.expression())
+        print(value)
+
+    def exitAssignmentStatement(self, ctx):
+        # Обрабатываем присваивание переменной
+        var_name = ctx.IDENTIFIER().getText()
+        value = self.evaluate_expression(ctx.expression())
+        variables[var_name] = value
+
+    def evaluate_expression(self, ctx):
+        if isinstance(ctx, OurLangParser.NumberExprContext):
+            return int(ctx.NUMBER().getText())
+        elif isinstance(ctx, OurLangParser.IdExprContext):
+            var_name = ctx.IDENTIFIER().getText()
+            return variables.get(var_name, 0)
+        elif isinstance(ctx, OurLangParser.AddSubExprContext):
+            left = self.evaluate_expression(ctx.expression(0))
+            right = self.evaluate_expression(ctx.expression(1))
+            return left + right if ctx.op.type == OurLangParser.PLUS else left - right
+        elif isinstance(ctx, OurLangParser.MulDivExprContext):
+            left = self.evaluate_expression(ctx.expression(0))
+            right = self.evaluate_expression(ctx.expression(1))
+            return left * right if ctx.op.type == OurLangParser.MUL else left // right
+        elif isinstance(ctx, OurLangParser.LogicalExprContext):
+            left = self.evaluate_expression(ctx.expression(0))
+            right = self.evaluate_expression(ctx.expression(1))
+            return int(left and right) if ctx.op.type == OurLangParser.AND else int(left or right)
+        elif isinstance(ctx, OurLangParser.NotExprContext):
+            return int(not self.evaluate_expression(ctx.expression()))
+        elif isinstance(ctx, OurLangParser.ParenExprContext):
+            return self.evaluate_expression(ctx.expression())
+        else:
+            raise ValueError("Unknown expression type")
 
 def main():
+    input_stream = StdinStream()
+    lexer = OurLangLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = OurLangParser(stream)
 
-    visitor = OurVisitor()
+    tree = parser.program()
 
-    while True:
-        input_text = input("enter text: ")
-        input_stream = InputStream(input_text)
-
-        lexer = OurLangLexer(input_stream)
-        stream = CommonTokenStream(lexer)
-        parser = OurLangParser(stream)
-
-        tree = parser.program()
-
-        visitor.visit(tree)
-
+    listener = OurLangPrintListener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
 
 if __name__ == '__main__':
     main()
